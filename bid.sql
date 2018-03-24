588c36d333985421779fe38d4086c63ff9abc7c8
mengeks <garethalexuni@gmail.com>
2018-03-24 12:14:05 +0800

    Merged all sql

--- bid.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE bid(
-phone_number INTEGER REFERENCES appuser(phone_number),
-rid_number VARCHAR(10) REFERENCES ride_generate(rid_number),
-status BOOLEAN,
-point INTEGER,
-PRIMARY KEY (phone_number,rid_number)
-);
