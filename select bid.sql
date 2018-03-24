4c2da44c111c697450985703af3049817be31a87
mengeks <garethalexuni@gmail.com>
2018-03-24 16:43:34 +0800

    Updated car, ride_generate, bid and all related data

--- select bid.sql
+++ select bid.sql
@@ -1,18 +1,18 @@
 
-# Select 90388714's bid to 'AA00000001'
+# Select 90388714's bid to 'AA00000003'
 begin;
 update ride_generate
 set passenger_id = 90388714
-where rid_number = 'AA00000001';
+where rid_number = 'AA00000003';
 
 update bid
 set status = TRUE
-where rid_number = 'AA00000001'
+where rid_number = 'AA00000003'
 and phone_number = 90388714;
 
 
 update bid
-where rid_number = 'AA00000001'
+where rid_number = 'AA00000003'
 and phone_number <> 90388714;
 
 commit;
