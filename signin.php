ea184077994a14a0d55692c874b0c5721c3cf5fb
Su Sumei <sumei.su@u.nus.edu>
2018-03-24 11:13:26 +0800

    add bootstrap and signin

--- signin.php
+++ signin.php
@@ -1,3 +1,7 @@
+<?php
+// Start the session
+session_start();
+?>
 <!DOCTYPE html>  
 <head>
   <title>Sign up</title>
@@ -5,33 +9,34 @@
   <style>li {list-style: none;}</style>
 </head>
 <body>
-  <h2>Sign up</h2>
+  <h2>Sign In</h2>
   <ul>
     <form name="display" action="signin.php" method="POST" >
       <li>Phone Number: </li>
       <li><input type="text" name="phone" /></li>
-      <li>Gender:</li>
-      <li>
-        <input type="radio" name="gender" value="M" checked> Male<br>
-        <input type="radio" name="gender" value="F"> Female<br>
-      </li>
-      <li><input type="submit" name="submit" /></li>
+      <li>Password:</li>
+      <li><input type="password" name="password" /></li>
+      <li><input type="submit" name="login" /></li>
     </form>
   </ul>
   <?php
+    if (isset($_POST['login'])) {
   	// Connect to the database. Please change the password in the following line accordingly
-    $db     = pg_connect("host=localhost port=5432 dbname=Team25 user=postgres password=postgres");	
-    $result = pg_query($db, "INSERT INTO appuser VALUES('$_POST[phone]', '$_POST[fname]', '$_POST[lname]', '$_POST[gender]', null)");		// Query template
-    $row    = pg_fetch_assoc($result);		// To store the result row
-    if (isset($_POST['submit'])) {
-        if (!$result) {
-            echo "Update failed!!";
-        } else {
-            echo "Update successful!";
-            header("Location: /cs2102/signin.php");
-            exit;
-        }
+      $db     = pg_connect("host=localhost port=5432 dbname=Team25 user=postgres password=postgres");	
+      $result = pg_query($db, "SELECT * FROM appuser WHERE phone_number='" .$_POST[phone]. "' AND password='" .$_POST[password]. "';");		// Query template
+      $row    = pg_fetch_assoc($result);		// To store the result row
+
+      if (!$row) {
+          echo "The user name or password is incorrect!";
+      } else {
+          echo "Logged in!";
+          $_SESSION["user_id"] = $row["phone_number"];
+          $_SESSION["password"] = $row["password"];
+          echo $_SESSION["user_id"];
+          header("Location: ./index.php");
+          exit;
+      }
     }
-    ?>  
+  ?>  
 </body>
 </html>
