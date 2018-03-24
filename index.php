17c2830f9a01c782ff715d2d7b269238d6cedb45
Su Sumei <sumei.su@u.nus.edu>
2018-03-24 12:11:36 +0800

    Add nav bar

--- index.php
+++ index.php
@@ -13,17 +13,38 @@ if (isset($_SESSION["user_id"])) {
   <title>Index</title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <style>li {list-style: none;}</style>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
+  <nav class="navbar navbar-expand-sm bg-warning navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Active Rides</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Current Bids</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="generaterides.php">Generate Rides</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="searchrides.php">Search Rides</a>
+    </li>
+  </ul>
+</nav>
   <?php
-    $db     = pg_connect("host=localhost port=5432 dbname=Team25 user=postgres password=Ml271slwmx"); 
+    $db     = pg_connect("host=localhost port=5432 dbname=Team25 user=postgres password=postgres"); 
     //$result = pg_query($db, "SELECT first_name, last_name FROM appuser WHERE phone_number = '" .$user_id. "';");
-    $result = pg_query($db, "SELECT first_name, last_name FROM appuser WHERE phone_number = '41514151';");  
+    $result = pg_query($db, "SELECT first_name, last_name FROM appuser WHERE phone_number = ' " .$user_id. "';");  
     
         if ($result){
           while ($row = pg_fetch_assoc($result)){
-            echo "<h2>Hello!".$row['first_name']."</h2>";
+            echo "<h2>Hello! ".$row['first_name']."</h2>";
           }
         }
         else{
