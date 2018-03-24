bb60ec057f2f7a621cb11d0f7aeed3bde45d2670
mengeks <garethalexuni@gmail.com>
2018-03-24 17:03:53 +0800

    MX latest change

--- select bid.sql
+++ select bid.sql
@@ -1,5 +1,5 @@
 
-# Select 90388714's bid to 'AA00000003'
+-- Select 90388714's bid to 'AA00000003'
 begin;
 update ride_generate
 set passenger_id = 90388714
@@ -12,6 +12,7 @@ and phone_number = 90388714;
 
 
 update bid
+set status = FALSE
 where rid_number = 'AA00000003'
 and phone_number <> 90388714;
 
